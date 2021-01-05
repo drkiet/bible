374fd351a658ab2967a41c7036ba25e1264b37a9
Kiet T. Tran, Ph.D <ktran@harrisburgu.edu>
2021-01-05 06:50:24 -0500

    Updated parsing bible

--- prepare-hadoop-data.bash
+++ prepare-hadoop-data.bash
@@ -1,10 +1,15 @@
 #!/bin/bash
 hdfs dfs -mkdir -p /user/thebible
-hdfs dfs -mkdir /tmp
 
-hdfs dfs -copyFromLocal ./ccc /user/thebible
 hdfs dfs -copyFromLocal ./dr /user/thebible
 hdfs dfs -copyFromLocal ./lv /user/thebible
-hdfs dfs -copyFromLocal ./metadata /user/thebible
 hdfs dfs -copyFromLocal ./nab /user/thebible
 hdfs dfs -copyFromLocal ./rsv /user/thebible
+
+hdfs dfs -copyFromLocal ./ccc /user/thebible
+hdfs dfs -copyFromLocal ./metadata /user/thebible
+
+hdfs dfs -copyFromLocal ./dr.csv /user/thebible
+hdfs dfs -copyFromLocal ./lv.csv /user/thebible
+hdfs dfs -copyFromLocal ./nab.csv /user/thebible
+hdfs dfs -copyFromLocal ./rsv.csv /user/thebible
